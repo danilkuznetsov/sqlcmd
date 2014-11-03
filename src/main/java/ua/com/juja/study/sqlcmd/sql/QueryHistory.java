9e9afdab102ae5e25c2507cebdc20d49d395812a
Viktor Kuchyn <kuchin.victor@gmail.com>
2014-11-03 23:25:10 +0800

    added Buffer size constant to interface

--- src/main/java/ua/com/juja/study/sqlcmd/sql/QueryHistory.java
+++ src/main/java/ua/com/juja/study/sqlcmd/sql/QueryHistory.java
@@ -7,6 +7,8 @@ package ua.com.juja.study.sqlcmd.sql;
  * Time: 11:23 PM
  */
 public interface QueryHistory {
+    public static final int QUERY_BUFFER_COUNT = 20;
+
     public abstract String getNextQuery();
 
     public abstract String getPreviousQuery();
