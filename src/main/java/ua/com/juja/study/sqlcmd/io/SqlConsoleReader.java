ecbe5ef4d11381d9c320a234df8d7303b98f329b
Viktor Kuchyn <kuchin.victor@gmail.com>
2014-11-18 01:05:15 +0800

    Left, right navigation

--- src/main/java/ua/com/juja/study/sqlcmd/io/SqlConsoleReader.java
+++ src/main/java/ua/com/juja/study/sqlcmd/io/SqlConsoleReader.java
@@ -15,6 +15,7 @@ public class SqlConsoleReader {
 
     public static final int SEMICOLON = 59;
     public static final int NEW_LINE = 10;
+    public static final int BACKSPACE = 127;
     private ConsoleReader consoleReader;
     private Writer writer;
     private CursorBuffer cursorBuffer;
@@ -33,6 +34,7 @@ public class SqlConsoleReader {
             int key = consoleReader.readVirtualKey();
             switch (key) {
                 case NEW_LINE:
+                    sb.append(cursorBuffer.toString());
                     if (previousKey == SEMICOLON) {
                         exit = true;
                     } else {
@@ -40,12 +42,16 @@ public class SqlConsoleReader {
                         newLine();
                     }
                     break;
-                case 127:
+                case BACKSPACE:
                     consoleReader.backspace();
-
+                    break;
+                case 2:
+                    consoleReader.moveCursor(-1);
+                    break;
+                case 6:
+                    consoleReader.moveCursor(1);
                     break;
                 default:
-                    sb.appendCodePoint(key);
                     cursorBuffer.write((char) key);
                     writer.write(key);
             }
@@ -58,33 +64,8 @@ public class SqlConsoleReader {
     private void newLine() throws IOException {
         consoleReader.moveCursor(0);
         consoleReader.printNewline();
-        cursorBuffer.getBuffer().setLength(0);
+        cursorBuffer.clearBuffer();
         cursorBuffer.cursor = 0;
-
     }
 
-//    private class ConsoleWriter extends PrintWriter{
-//        private StringBuilder input = new StringBuilder();
-//        public ConsoleWriter(Writer out) {
-//            super(out);
-//        }
-//
-//        @Override
-//        public void write(int codePoint) {
-//            super.write(codePoint);
-//            input.appendCodePoint(codePoint);
-//        }
-//
-//        @Override
-//        public void write(char[] buf) {
-//            super.write(buf);
-//            input.append(buf);
-//        }
-//
-//        public String getInputAndClear(){
-//            String res = input.toString();
-//            input = new StringBuilder();
-//            return res;
-//        }
-//    }
 }
