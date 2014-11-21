e81a19bef993742960106c7675f0d4e0d5649fdc
Viktor Kuchyn <kuchin.victor@gmail.com>
2014-11-21 12:11:04 +0800

    Added DI interface

---
+++ src/main/java/ua/com/juja/study/sqlcmd/di/ApplicationContext.java
@@ -0,0 +1,20 @@
+package ua.com.juja.study.sqlcmd.di;
+
+import ua.com.juja.study.sqlcmd.database.DatabaseExecutor;
+import ua.com.juja.study.sqlcmd.engine.KeyboardManager;
+import ua.com.juja.study.sqlcmd.sql.QueryHistory;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: viktor
+ * Date: 11/19/14
+ * Time: 2:14 PM
+ */
+public interface ApplicationContext {
+
+    public QueryHistory getQueryHistory();
+
+    public DatabaseExecutor getDatabaseExecutor();
+
+    public KeyboardManager getKeyboardManager();
+}
