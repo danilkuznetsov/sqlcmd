03aec7e1e0ed0a12648450ba276b8b2948b4fc75
Viktor Kuchyn <kuchin.victor@gmail.com>
2014-11-21 11:40:23 +0800

    Dependency injection engine

--- src/main/java/ua/com/juja/study/sqlcmd/di/DefaultApplicationContext.java
+++ src/main/java/ua/com/juja/study/sqlcmd/di/DefaultApplicationContext.java
@@ -1,15 +1,11 @@
 package ua.com.juja.study.sqlcmd.di;
 
 import ua.com.juja.study.sqlcmd.config.SqlCmdConfig;
-import ua.com.juja.study.sqlcmd.database.AsyncDatabaseExecutor;
 import ua.com.juja.study.sqlcmd.database.DatabaseExecutor;
 import ua.com.juja.study.sqlcmd.database.mock.MockDatabaseExecutor;
 import ua.com.juja.study.sqlcmd.engine.KeyboardManager;
 import ua.com.juja.study.sqlcmd.sql.ArrayQueryHistory;
 import ua.com.juja.study.sqlcmd.sql.QueryHistory;
-
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 /**
  * Created with IntelliJ IDEA.
@@ -23,12 +19,10 @@ public class DefaultApplicationContext implements ApplicationContext {
     private QueryHistory queryHistory;
     private DatabaseExecutor databaseExecutor;
     private KeyboardManager keyboardManager;
-    private ExecutorService executorService;
 
     public DefaultApplicationContext(SqlCmdConfig config) {
         this.config = config;
-        executorService = Executors.newFixedThreadPool(5);
-        databaseExecutor = new AsyncDatabaseExecutor(new MockDatabaseExecutor());
+        databaseExecutor = new MockDatabaseExecutor();
         queryHistory = new ArrayQueryHistory();
         keyboardManager = new KeyboardManager(queryHistory, databaseExecutor);
     }
@@ -47,15 +41,4 @@ public class DefaultApplicationContext implements ApplicationContext {
     public KeyboardManager getKeyboardManager() {
         return keyboardManager;
     }
-
-    public ExecutorService getExecutorService() {
-        return executorService;
-    }
-
-    @Override
-    public void shutdown() {
-        executorService.shutdown();
-    }
-
-
 }
